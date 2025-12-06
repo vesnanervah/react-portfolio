e2088a05390f4d6fbea18aa9a5d03e2ca0697ebd
Kirill Zverev <vesnanervakh@gmail.com>
2025-12-06 15:08:26 +0300

    remove todo

--- app/lib/ui/home-sections/pricing-section.tsx
+++ app/lib/ui/home-sections/pricing-section.tsx
@@ -7,7 +7,6 @@ export default function PricingSection() {
         <SectionHeader header="Packages & Pricing" color={SectionHeaderColor.black} className="pb-10">
             Choose the package that suits your story. All options are customizable to your needs.
         </SectionHeader>
-        {/*TODO: There are should be either 4, 2 or 1 elements in the row*/}
         <div className="max-w-[660px] 2xl:max-w-max mx-auto flex flex-wrap gap-5 2xl:gap-10 justify-center">
             {
                 pricing.map((p) => {
