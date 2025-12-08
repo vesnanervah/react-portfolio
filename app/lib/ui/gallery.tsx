56cea3cd7d6b317f9fa803f2e16190a6f90744be
Kirill Zverev <vesnanervakh@gmail.com>
2025-12-08 13:55:24 +0300

    feat: add scrolling to gallery

--- app/lib/ui/gallery.tsx
+++ app/lib/ui/gallery.tsx
@@ -1,32 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
-import { useReducer, useState } from "react";
-import { GalleryImage, GalleryImageAlign } from "../data/gallery-images";
-import clsx from "clsx";
+import { useReducer } from "react";
+import { GalleryImage, GalleryImageAlign, galleryImages } from "../data/gallery-images";
+import WindowSizeListener from "react-window-size-listener";
 
 const slidableId = "slidable"
+
 const sliderInitialPos = 0;
+const sliderNormalMovement = 300;
 
 export default function Gallery(props: GalleryProps) {
     const [sliderPos, dispatchSliderPos] = useReducer(sliderPosReducer, sliderInitialPos);
 
 
     return <div className={`w-screen overflow-hidden flex flex-col items-center ${props.className ?? ''}`}>
+
         {/*Images area*/}
-        <div id={slidableId} className={clsx("w-max h-[654px] relative flex gap-5 pl-5 pr-5 shrink-0 transition-all duration-200", `translate-x-[${sliderPos}px]`)}>
+        <div id={slidableId} className={'w-max h-[654px] relative flex gap-5 pl-5 pr-5 shrink-0 transition-all duration-200'} style={{
+            transform: `translateX(${sliderPos}px)`
+        }}>
             {
                 props.images.map(Item)
             }
         </div>
+
         {/*Gestures area*/}
         <div className="h-[654px] w-screen bottom-[654px] relative grid grid-cols-[3fr_4fr_3fr]">
             <div className="min-h-max min-w-max" onClick={() => dispatchSliderPos(SliderMoveDirection.left)}></div>
             <div className="min-h-max min-w-max" onClick={() => dispatchSliderPos(SliderMoveDirection.initial)}></div>
             <div className="min-h-max min-w-max" onClick={() => dispatchSliderPos(SliderMoveDirection.right)}></div>
         </div>
+
+        <WindowSizeListener onResize={onWindowResize}></WindowSizeListener>
     </div>
 
     function Item(i: GalleryImage) {
-        return <div key={i.src} className={`flex flex-col h-p[654px] ${getAlignClassName(i)}`}>
+        return <div key={i.src} id={`gallery-image-${galleryImages.indexOf(i)}`} className={`flex flex-col h-p[654px] ${getAlignClassName(i)}`}>
             <img src={i.src} alt="An photo from gallery"></img>
         </div>
     }
@@ -43,22 +51,47 @@ export default function Gallery(props: GalleryProps) {
     }
 
     function sliderPosReducer(state: number, action: SliderMoveDirection): number {
+        if (document.getElementById(slidableId)!.offsetWidth < window.innerWidth) {
+            // No need to scroll
+            return sliderInitialPos
+        }
+
         switch (action) {
             case SliderMoveDirection.initial:
                 return sliderInitialPos;
             case SliderMoveDirection.left:
-                return calculateNextLeftPos()
+                return calculateNextLeftPos(state)
             case SliderMoveDirection.right:
-                return calculateNextRightPos()    
+                return calculateNextRightPos(state)    
         }
     }
 
-    function calculateNextLeftPos() {
-        return sliderInitialPos
+    function calculateNextLeftPos(currentPos: number) {
+        const windowWidth = window.innerWidth
+        // After checkup above we know for sure that offsetWidth >  innerWidth
+        // Because the moving part is initially aligned in center, the unseen part from left or from right are equal
+        const initiallyUnseenRightWidth = (document.getElementById(slidableId)!.offsetWidth - windowWidth) / 2
+        // (windowWidth / 2) - (document.getElementById(`gallery-image-${galleryImages.length - 1}`)!.offsetWidth / 2) means
+        // we want not only be able scroll unit we can see last element but also be able to center it
+        const edgeScrollPos = initiallyUnseenRightWidth + (windowWidth / 2) - (document.getElementById(`gallery-image-${galleryImages.length - 1}`)!.offsetWidth / 2)
+        if (edgeScrollPos <= Math.abs(currentPos - sliderNormalMovement)) {
+            return -edgeScrollPos;
+        }
+        return currentPos - sliderNormalMovement;
     }
 
-    function calculateNextRightPos() {
-        return sliderInitialPos
+    function calculateNextRightPos(currentPos: number) {
+        const windowWidth = window.innerWidth
+        const initiallyUnseenRightWidth = (document.getElementById(slidableId)!.offsetWidth - windowWidth) / 2
+        const edgeScrollPos = initiallyUnseenRightWidth + (windowWidth / 2) - (document.getElementById(`gallery-image-0`)!.offsetWidth / 2)
+        if (edgeScrollPos <= Math.abs(currentPos + sliderNormalMovement)) {
+            return edgeScrollPos;
+        }
+        return currentPos + sliderNormalMovement;
+    }
+
+    function onWindowResize() {
+        dispatchSliderPos(SliderMoveDirection.initial)
     }
 }
 
